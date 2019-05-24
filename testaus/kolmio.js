c27fa560a8a0f590177b3ca12b2ec5d084e8dfdf
KalleVuoristo <42265593+KalleVuoristo@users.noreply.github.com>
2019-05-24 12:50:35 +0300

    Update kolmio.js

--- testaus/kolmio.js
+++ testaus/kolmio.js
@@ -1,22 +1,21 @@
 function kolmio(a,b) {
    
-    if(a <=-0){
-        throw new Error('Negatiivisellä luvulla ei onnistu. Anna positiivinen luku');
+    if(a <=0){
+        throw new Error('Negatiivisella luvulla ei onnistu. Anna positiivinen luku');
     }
     
-    if(b <=-0){
-        throw new Error('Negatiivisellä luvulla ei onnistu. Anna positiivinen luku');
+    if(b <=0){
+        throw new Error('Negatiivisella luvulla ei onnistu. Anna positiivinen luku');
     }
     
-    
+
     let pintaala = Number(a)*Number(b)/2;
     if(Number.isNaN(pintaala)){
         throw new Error('ei lukuja, anna kolmion kanta ja korkeus');
     }
 
 
-   return a * b / 2;
-
+    return a * b / 2;
 
 
 }
