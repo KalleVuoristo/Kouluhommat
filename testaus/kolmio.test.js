8f8f29185feb8ba1184be7ecc12ded5c97c6fb87
KalleVuoristo <42265593+KalleVuoristo@users.noreply.github.com>
2019-05-24 12:28:28 +0300

    Update kolmio.test.js

--- testaus/kolmio.test.js
+++ testaus/kolmio.test.js
@@ -9,12 +9,12 @@ test('Laskee kolmion pinta-alan', () => {
     expect(kolmio(25,5)).toBe(kolmio);
 });
 
-test('yritetään jakaa negatiivisella luvulla, pyydetään jokin positiivinen luku', () => {
+test('Yritetään negatiivisella luvulla, pyydetään jokin positiivinen luku', () => {
     expect(() => {kolmio(4,-13)}).toThrow('Negatiivisellä luvulla ei onnistu. Anna positiivinen luku');
     
 });
 
-test('yritetään jakaa negatiivisella luvulla, pyydetään jokin positiivinen luku', () => {
+test('Yritetään negatiivisella luvulla, pyydetään jokin positiivinen luku', () => {
     expect(() => {kolmio(-4,4)}).toThrow('Negatiivisellä luvulla ei onnistu. Anna positiivinen luku');
     
 });
