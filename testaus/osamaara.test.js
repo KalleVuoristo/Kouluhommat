780c11d5ddf1fb2492369af27b404330b3d93442
KalleVuoristo <42265593+KalleVuoristo@users.noreply.github.com>
2019-05-24 12:51:32 +0300

    Update osamaara.test.js

--- testaus/osamaara.test.js
+++ testaus/osamaara.test.js
@@ -1,25 +1,25 @@
 const osamaara = require('./osamaara');
 
 test('suorittaa numeroiden jakamisen ja tarkistaa onko oikein, tulos 2', () => {
-    expect(osamaara(22,2)).toBe(osamaara);
+expect(osamaara(22,2)).toBe(11);
 });
 
 test('syötteet numeromaisia merkkijonoja "6"+"3", tulos 2', () => {
-    expect(osamaara("6","3")).toBe(osamaara);
+expect(osamaara("6","3")).toBe(2);
 
 });
 
 test('syötteet ei numeromaisia merkkijonoja "jakolaskuneka"+"jakolaskuntoka", heittää poikkeuksen', () => {
-    expect(() => {osamaara("jakolaskuneka","jakolaskuntoka")}).toThrow('ei lukuja');
+expect(() => {osamaara("jakolaskuneka","jakolaskuntoka")}).toThrow('ei lukuja');
     
 });
 
 test('ei anneta yhtään parametriä, pyydetään parametri', () => {
-    expect(() => {osamaara()}).toThrow('ei parametria, anna jokin parametri');
+expect(() => {osamaara()}).toThrow('ei parametria, anna jokin parametri');
     
 });
 
 test('yritetään jakaa nollalla, pyydetään jokin luku', () => {
-    expect(() => {osamaara(4,0)}).toThrow('nollalla jakaminen ei onnistu');
+expect(() => {osamaara(4,0)}).toThrow('nollalla jakaminen ei onnistu');
     
 });
